71207de06072b9e1358081e861cbe7d2f01672d0
Scott Ouellette <scottx611x@gmail.com>
2018-12-04 16:16:17 -0500

    Update lambda function to generate a dummy .zip for testing

--- lambda/isa-tab-exporter.py
+++ lambda/isa-tab-exporter.py
@@ -1,7 +1,24 @@
-import json
+import base64
+from io import BytesIO
+from zipfile import ZipFile
+
 
 def post_handler(event, context):
+    bytes_in_memory = BytesIO()
+    z = ZipFile(bytes_in_memory, "a")
+    z.writestr("i_investigation.txt", "Investigation")
+    z.writestr("s_study.txt", "Study")
+    z.writestr("a_assay.txt", "Assay")
+    z.close()
+    bytes_in_memory.seek(0)
+    encoded_payload = base64.b64encode(bytes_in_memory.read())
+
     return {
         "statusCode": 200,
-        "body": json.dumps({'event': event})
+        "headers": {
+          "Content-Type": "application/zip",
+          "Content-Encoding": "zip"
+        },
+        "body": encoded_payload.decode('ascii'),
+        "isBase64Encoded": True
     }
