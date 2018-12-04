90e51a2c09b61e6ea69caa8bb60c394067ef9b60
Scott Ouellette <scottx611x@gmail.com>
2018-12-04 10:24:08 -0500

    Remove non essential output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,3 @@
-output "api_gateway_deployment_execution_arn" {
-  value = "${module.api_gateway.aws_api_gateway_rest_api_execution_arn}"
-}
-
 output "api_gateway_deployment_invoke_url" {
   value = "${module.api_gateway.api_gateway_deployment_invoke_url}"
 }
