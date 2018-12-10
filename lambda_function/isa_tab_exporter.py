e6f9d737f951f542d1ff6f807955be598e77def7
Scott Ouellette <scottx611x@gmail.com>
2018-12-10 15:53:43 -0500

    Tiday up lambda post handler

--- lambda_function/isa_tab_exporter.py
+++ lambda_function/isa_tab_exporter.py
@@ -8,18 +8,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-def post_handler(event, context):
-    isatab_filename = "ISATab"
-    body = event.get("body")
-
-    if body is not None:
-        isatab_filename = json.loads(body).get("isatab_filename")
-
-    # TODO: error handling
-
-    logger.info(f"Event: {event}")
-    logger.info(f"Context: {context}")
-
+def create_test_zip():
     bytes_in_memory = BytesIO()
     z = ZipFile(bytes_in_memory, "a")
     z.writestr("i_investigation.txt", "Investigation")
@@ -27,15 +16,30 @@ def post_handler(event, context):
     z.writestr("a_assay.txt", "Assay")
     z.close()
     bytes_in_memory.seek(0)
-    encoded_payload = base64.b64encode(bytes_in_memory.read())
+    return base64.b64encode(bytes_in_memory.read())
 
+
+def create_response(isatab_filename, status_code=200):
     return {
-        "statusCode": 200,
+        "statusCode": status_code,
         "headers": {
           "Content-Type": "application/zip",
           "Content-Encoding": "zip",
           "Content-Disposition": f"attachment; filename=\"{isatab_filename}.zip\""
         },
-        "body": encoded_payload.decode('ascii'),
+        "body": create_test_zip().decode('ascii'),
         "isBase64Encoded": True
     }
+
+def post_handler(event, context):
+    isatab_filename = "ISATab"
+    body = event.get("body")
+
+    logger.info(f"Event: {event}")
+
+    if body is not None:
+        isatab_filename = json.loads(body).get("isatab_filename")
+
+    # TODO: error handling
+
+    return create_response(isatab_filename)
