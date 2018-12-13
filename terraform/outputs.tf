e194a5af72b3c39800d3996e712254b21c75306b
Scott Ouellette <scottx611x@gmail.com>
2018-12-13 15:03:58 -0500

    Update api_gateway_invoke_url output to include resource path

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,3 @@
 output "api_gateway_deployment_invoke_url" {
-  value = "${module.api_gateway.api_gateway_deployment_invoke_url}"
+  value = "${module.api_gateway.api_gateway_deployment_invoke_url}${module.api_gateway.api_gateway_resource_path}"
 }
