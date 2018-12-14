f4a6e8902e6bf2c4f789973541367f3b5de616ce
Scott Ouellette <scottx611x@gmail.com>
2018-12-14 13:12:52 -0500

    `terraform fmt` changes

--- terraform/modules/lambda/vars.tf
+++ terraform/modules/lambda/vars.tf
@@ -1,12 +1,14 @@
 variable api_gateway_rest_api_execution_arn {}
 variable api_gateway_resource_path {}
 variable iam_role_arn {}
+
 variable lambda_memory_size {
   # Note: AWS Lambda allocates proportional CPU power, network bandwidth,
   # and disk I/O based off of the specified `memory_size`
   # See `Flexible resource model` here: https://aws.amazon.com/lambda/features/
   default = 2048
 }
+
 variable lambda_zip_hash {}
 variable lambda_zip_name {}
 variable lambda_zip_s3_object {}
