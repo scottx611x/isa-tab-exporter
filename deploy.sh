b2ad16bdceae73f2fb4b97939d87a9b3fe14c475
Scott Ouellette <scottx611x@gmail.com>
2018-12-14 13:06:58 -0500

    Force reinstall lambda reqs on deploy

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,10 @@
 # install lambda's python requirements
 docker run -it -v "$PWD/lambda_function":/lambda_build_dir \
     scottx611x/aws-linux-python-3.6 \
-    /usr/bin/pip-3.6 install -r /lambda_build_dir/requirements.txt -t /lambda_build_dir/__python_reqs__
+        /usr/bin/pip-3.6 install \
+        --force-reinstall \
+        -r /lambda_build_dir/requirements.txt \
+        -t /lambda_build_dir/__python_reqs__
 
 # initialize Terraform and apply infrastructure changes
 cd terraform
