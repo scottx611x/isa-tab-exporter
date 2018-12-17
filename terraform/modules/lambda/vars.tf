2dee3f782b34a40da516b534cafc588acb7661a8
Scott Ouellette <5629547+scottx611x@users.noreply.github.com>
2018-12-16 21:49:35 -0500

    Update vars.tf

--- terraform/modules/lambda/vars.tf
+++ terraform/modules/lambda/vars.tf
@@ -6,7 +6,7 @@ variable lambda_memory_size {
   # Note: AWS Lambda allocates proportional CPU power, network bandwidth,
   # and disk I/O based off of the specified `memory_size`
   # See `Flexible resource model` here: https://aws.amazon.com/lambda/features/
-  default = 2048
+  default = 3008
 }
 
 variable lambda_zip_hash {}
