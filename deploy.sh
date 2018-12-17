7cbf50cbe0e6951cc10b885d08f9af48e56673cb
Scott Ouellette <scottx611x@gmail.com>
2018-12-17 10:56:02 -0500

    Move docker script into docker-context

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# install lambda's python requirements
-docker run -it -v "$PWD/lambda_function":/lambda_build_dir \
-    scottx611x/aws-linux-python-3.6 \
-        /usr/bin/pip-3.6 install \
-        -r /lambda_build_dir/requirements.txt \
-        -t /lambda_build_dir/__python_reqs__
+./docker_context/install-lambda-reqs.sh $PWD
 
 # initialize Terraform and apply infrastructure changes
 cd terraform
---
+++ docker_context/install-lambda-reqs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Usage ./install-lambda-reqs.sh <absolute_path_to_lambda_function_directory>
+
+
+# install lambda's python requirements
+docker run -it -v "$1/lambda_function":/lambda_build_dir \
+    scottx611x/aws-linux-python-3.6 \
+        /usr/bin/pip-3.6 install \
+        -r /lambda_build_dir/requirements.txt \
+        -t /lambda_build_dir/__python_reqs__
