c2d960f59d601a47a3813d7fe67674c7371ddf46
Scott Ouellette <scottx611x@gmail.com>
2018-12-19 09:50:43 -0500

    Pass absolute path to lambda_function dir to docker script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./docker_context/install-lambda-reqs.sh $PWD
+./docker_context/install-lambda-reqs.sh $PWD/lambda_function
 
 # initialize Terraform and apply infrastructure changes
 cd terraform
