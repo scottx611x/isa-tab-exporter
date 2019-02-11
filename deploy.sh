da0a2b1b9a9363cd19b8f2f686b9c02db6c4d2c3
Scott Ouellette <scottx611x@gmail.com>
2019-02-11 14:27:33 -0500

    Update variable interpolation in deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,8 @@ cd terraform
 terraform init -input=false
 terraform workspace select production
 terraform apply -auto-approve \
-    -var 'acm_certificate_arn=$ACM_CERTIFICATE_ARN' \
-    -var 'domain_name=isa-tab-exporter.aws.stemcellcommons.org' \
-    -var 'hosted_zone_id=$ROUTE_53_HOSTED_ZONE_ID' \
-    -var 'api_gateway_stage_name=production'
+    -var "acm_certificate_arn=$ACM_CERTIFICATE_ARN" \
+    -var "domain_name=isa-tab-exporter.aws.stemcellcommons.org" \
+    -var "hosted_zone_id=$ROUTE_53_HOSTED_ZONE_ID" \
+    -var "api_gateway_stage_name=production"
 cd ..
