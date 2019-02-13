02c84219e16f34f3245eee66b90238df855c223a
Scott Ouellette <scottx611x@gmail.com>
2019-02-13 09:49:48 -0500

    Resort back to regular API Gateway stage name; I think these have to be valid within a URL and the same constraints don't apply to a workspace name

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -28,7 +28,7 @@ locals {
 module api_gateway {
   source                     = "./modules/api_gateway"
   acm_certificate_arn        = "${var.acm_certificate_arn}"
-  api_gateway_stage_name     = "${local.resource_name_prefix}${var.api_gateway_stage_name}"
+  api_gateway_stage_name     = "${var.api_gateway_stage_name}"
   domain_name                = "${var.domain_name}"
   lambda_function_invoke_arn = "${module.lambda.lambda_function_invoke_arn}"
   resource_name_prefix       = "${local.resource_name_prefix}"
